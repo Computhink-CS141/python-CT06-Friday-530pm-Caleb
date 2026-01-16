6f48e2bc861af32beb63b5dc0c862761af2d178b
Computhink-CS141 <cs141@computhink.com.sg>
2026-01-16 10:47:15 +0000

    end of lesson 2

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,70 @@
 print("Hello from lesson 2")
+print("I like")
+# print("Red")
+# print("Orange")
+# print("Yellow")
+# print("Green")
+print("Blue")
+# print("Indigo")
+# print("Violet")
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
+# repeat 10 times
+# say hey
+# move 10 steps
+
+
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+# set counter to 0
+# repeat until the counter is 50
+# move 10 steps
+# turn clockwise 15 degrees
+# change counter by 10
+
+
+# ask what is your age and wait
+# if the answer is less than 18 
+# say access denied!
+# else
+# say welcome
+
+
+
+# accept one trash item from user
+# scan trash
+# if the trash is glass
+# put trash in glass bin
+# if the trash is plastic
+# put trash in plastic bin
+# if the trash is paper
+# put trash in paper bin
+# if the trash is not recyclable
+# put trash in non-recycleable bin
+
+
+# set password to passme
+# ask user for password
+# set to answer
+# if answer is passme
+# say congradulations
+# else
+# send error message to user
+
+
+
+
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+results =num1 + num2
+print("add")
+print(num1 + num2)
+
+print("multiply")
+print(num1 * num2)
