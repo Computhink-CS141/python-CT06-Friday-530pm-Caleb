e7b5dfc3a4e189909c9deeec76e977b36e5765d9
Computhink-CS141 <cs141@computhink.com.sg>
2026-02-06 10:45:59 +0000

    End of lesson 5

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,61 @@
 print("Hello from lesson 5")
+
+# age = input("What is the age of the person you are sending this message to?")
+# age = str(age)
+
+# name = input("What is the name of te person you are sending this to?")
+# name = str(name)
+
+# message = input("What is the message you want to send?")
+# message = str(message)
+
+# print("Happy " + age + "th birthday " + name + "! " + message)
+
+
+
+# for count in range(1000000000):
+#     print("I like Minecraft!")
+
+# for count in range(1000000000):
+#     print("I like Minecraft!", count)
+#     print("Let me play more often!!")
+
+
+# for count2 in  range(2 , 101 , 2):
+#     print(count2)
+
+
+# for count3 in range(0 , 59 ,1):
+#     print(count3)
+
+# for count4 in range(1 , 6 ,1):
+    # print(count4)
+
+# for count in range(51 , 101 ,1):
+#     print(count)
+
+# for count in range(18 , 30 ,1):
+#     print(count)
+
+
+# for count in range(2 , 23 ,2):
+#     print(count)
+
+# for count in range(8 , 97 ,8):
+#     print(count)
+
+# for count in range(5 , 0 ,-1):
+    # print(count)
+
+
+name = input ("What is your name?")
+for letter in name:
+    print("give me a " , letter.upper(), "!")
+
+
+
+
+
+
+
+
