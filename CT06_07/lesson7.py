b0e6b5ef5f6f0f566503cd3e815ae0b48d5a893a
Computhink-CS141 <cs141@computhink.com.sg>
2026-02-12 11:02:00 +0000

    end of lesson 7

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,124 @@
 print("Hello from lesson 7")
 
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+
+
+
+
+# Task 1
+
+# for num in range(1,11,1):
+#     print(num)
+
+
+# Task 2
+
+# for num in range(2,21,1):
+    # print(num)
+
+# Task 3
+
+# for num in range(10,0,-1):
+#     print(num)
+
+
+# Task 4
+
+# word = input("What word would you like to repeat?")
+# num = input("How many times would you like to repeat?")
+
+# for ans in range(int(num)):
+#     print(word)
+
+# Task 5
+
+# name1 = input("What is your name?")
+# num = input("How many times would you like to repeat?")
+
+# for ans in range(int(num)):
+#     print("Nice to meet you, " + name1)
+
+# Task 6
+
+# total = 0
+# for i in range(1,6,1):
+#     numi = input("What is number " + str(i ) + (": "))
+#     total = int(total) + int(numi)
+
+
+
+# print("Sum of the numbers is " + str(total))
+
+
+# Task 7
+
+# num = input("What number for the timestable?")
+
+# for i in range(1, 13, 1):
+    # print(int(num) , ("x") ,  i  , "=" , int(num)*i )
+
+# Task 8
+
+# num = input("Choose a number: ")
+# for i in range(1, 6 , 1):
+#     num= str(num)
+#     print(num*i)
+#     num= int(num) +1
+
+# Task 9
+
+# total = 0
+# for i in range(1,6,1):
+#     numi = input("What is number " + str(i ) + (": "))
+#     total = int(total) + int(numi)
+# total = int(total) / 5
+# print(str(total))
+
+# Task 10
+
+# num1 = input("How many students?")
+# num1= int(num1)+1
+# total = 0
+# for i in range(1,num1,1):
+#     num2 = input("What is the marks? "  + (": "))
+#     total = int(total) + int(num2)
+
+# print(int(total))
+
+# Task 11
+
+# hnum = input("Choose a number.")
+# hnum = int(hnum)+1
+# for i in range(1,hnum, 1):
+#     print("!"*i)
+
+# hnum = int(hnum)-2
+# for i in range(hnum,0, -1):
+#     print("!"*i)
+
+# Task 12
+
+number = int(input ("Type a number."))
+for i in range(number+1):
+    spaces = number - i
+    print((""*spaces )+ "#")
+
+
+
+
+
+
+
+
+
