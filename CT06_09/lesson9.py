ccf8858fecc6b41396e9ff181487fdbfbf5179a6
Computhink-CS141 <cs141@computhink.com.sg>
2026-03-20 10:58:47 +0000

    end of lesson 9

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,42 @@
 print("Hello from lesson 9")
+
+# import random
+# num1 = random.randint (1,6)
+# num2 = random.randint (1,6)
+# num3 = random.randint (1,6)
+
+# print("1st number:" + str(num1))
+# print("2nd number:" + str(num2))
+# print("3rd number:" + str(num3))
+
+# test1 = num1 % 2 == 0
+# test2 = num2 % 2 == 0
+# test3 = num3 % 2 == 0
+
+# print ("All numbers are even/odd:" , test1 == test2 == test3)
+
+# Task 2
+
+# length = int(input ("How many days did you borrow this book?"))
+
+# if length >= 25:
+#     print("Remember to return this book!")
+
+
+# Task 3
+
+apple = 1
+numa = int(input("How much apples do you want to buy?"))
+if numa >= 10:
+    print("You will get a discount!")
+    discount = 10/100 * (numa*apple)
+total = apple * numa - discount
+print ("The total cost:$"  +str(total))
+
+
+
+
+
+
+
+
