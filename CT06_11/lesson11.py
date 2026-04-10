e4252499ea1dc0be08d65b89b30969562b6921f3
Computhink-CS141 <cs141@computhink.com.sg>
2026-04-10 10:54:30 +0000

    end of lesson 11

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,99 @@
 print("Hello from lesson 11")
+
+# Recap 1
+# px = int(input("What is the price?"))
+# if px <= 5:
+#     print("Sounds good!")
+# elif px <= 50:
+#     print("Are you sure you really need this?")
+# elif px <= 500:
+#     print("Where are you getting this money from?!")
+# else:
+#     print("Don't even think about it!")
+
+# Task 1
+
+# rider1 = 125
+# rider2 = 150
+# if rider1 > 120 and rider2 >120:
+#     print("Ride")
+# else:
+#     print("Get out")
+
+# Task 2
+
+# num = int(input("Choose a number: "))
+# if num % 3 == 0 and num % 7 == 0:
+#     print("The number is divisible ny 3 and 7...")
+# else:
+#     print("Not divisible by 3 and 7.")
+
+# Task 3
+
+# firstname = input("What is your first name: ")
+# lastname = input("What is your last name: ")
+# if firstname == "James" and lastname == "Leong":
+#     print("YOU ARE WANTED!!!!!!")
+# else:
+#     print("YOU ARE FREE!!!!!!")
+
+# Task 4
+
+# rider1 = 25
+# rider2 = 6
+
+# if rider1 >= 18 or rider2>= 18:
+#     print("RIDE")
+# else:
+#     print("GET OUT")
+
+# Task 5
+
+# age = int(input("What is your age?"))
+# if age<12 or age >65:
+#     print("The ticket price is $15")
+# else:
+#     print("The price is $20")
+
+# Task 6
+
+# ans = input("Write something")
+
+# if ans == "M" or ans == "MALE":
+#     print("Valid input")
+# else:
+#     print("Invalid input")
+
+# Task 7
+
+# colour = input("Choose a colour: ")
+# if not colour == "green":
+#     print("Try again")
+# else:
+#     print("Great")
+
+# Task 8
+
+# day = input("What day is it?")
+
+# if not day == "Saturday":
+#     print("It is not Saturday!")
+# else:
+#     print("It is not Saturday.")
+
+# Task 9
+
+# passw = input("What is the password?")
+# if not passw == "Python123":
+#     print("Access denied")
+# else:
+#     print("Access allowed")
+
+
+
+
+
+
+
+
+
