2141d22ce4757f5ffd4f43d2a579d4c4db6c5f18
Computhink-CS141 <cs141@computhink.com.sg>
2026-04-17 10:57:15 +0000

    end of lesson 12

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,84 @@
 print("Hello from lesson 12")
+
+# Recap 1
+
+# game_status = "active"
+# if game_status == "active" or not game_status == "paused":
+#     print("Game in progress...")
+# else:
+#     print("paused")
+
+# Task 1A
+
+# visitors = 0
+# while visitors < 50:
+#     visitors = visitors+1
+#     print(visitors)
+
+# Task 1B
+
+# visitors = 18
+# while visitors < 30:
+    # visitors = visitors+1
+    # print(visitors)
+
+# Task 1C
+
+# visitors = 4
+# while visitors < 25:
+#     visitors = visitors+1
+#     print(visitors)
+
+# Task 2
+
+# visitors = 0
+# while visitors < 50:
+#     if  visitors == 30:
+#         break
+#     visitors = visitors+1
+#     print(visitors)
+
+# Task3
+
+# order = ""
+# while True:
+#     reply = input("What do you want to order?")
+#     if reply == "end":
+#         break
+#     order = order +", "+ reply
+# print("You ordered " + order)
+
+# Task 4A
+
+# num = 10
+# while num > 0:
+#     print(num)
+#     num = num -1
+# else:
+#     print("Happy new year!")
+
+# Task 4B
+
+# num = 10
+# while True:
+#     if num == 5:
+#         break
+#     print(num)
+#     num = num -1
+# else:
+#     print("Happy new year!")
+
+# Task 5
+
+import random
+while True:
+    num1 = random.randit (1,10)
+    num2 = random.randit (1,10)
+    ans = num1 +num2
+    user_ans = int(input("What is " , num1  num2 "?"))
+
+
+
+
+
+
