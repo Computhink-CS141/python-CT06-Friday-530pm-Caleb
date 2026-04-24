88f2bf53d1a2eccebc6b388fedfd81acb6b67212
Computhink-CS141 <cs141@computhink.com.sg>
2026-04-24 10:51:48 +0000

    end of lesson 13

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,55 @@
 print("Hello from lesson 13")
+
+# Recap 1
+# num= 25
+# while num <50:
+    # print(num)
+    # num = num+1
+
+# Recap 2
+# num = 100
+# while not num == 79:
+#     print(num)
+#     num = num-1
+
+# Recap 3
+# num = 5
+# while num <= 100:
+    # print(num)
+    # num = num+5
+
+# Task 1
+
+# items = ["Apples", "Bread", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Honey"]
+# items [7] = "Herbs"
+# items.append("Ice")
+# items.insert(1, "Banana")
+# del(items[2])
+# for abc in items:
+#     print(items)
+
+# Task 2
+
+# items= ["Apples", "Carrots", "Grapes"]
+# for abc in items:
+#     if abc == "Apples":
+#         print("I need 5 of these.")
+#     if abc == "Carrots":
+#         print("I need 3 of these.")
+#     if abc == "Grapes":
+#         print("Get the FarmFresh brand.")
+
+# extra
+
+# names = ["AMELIA CHEUNG CHEN XUAN","AUDREY NG WAN KI","AXEL SEETHO ANSHENG","CALEB LIEW EN LE","CHEN JING","CHEN YIJIE AYDEN","CHEW XUAN YING","CLARA YEE SING JING","DANISH ADAM BIN IRWAN SHARIN","DINESH BABU PRANAAV","EBENEZER CHEONG JAE KAI","ELSA YONG","EVIE TAN SHI JIE","FARHAN BIN YUZRI","GIDEON TEO JING WEI","GU YUCHEN LAWRENCE","HE JIXUN","HU MOTONG","JAMIE NG E-WEN","KAYLA YEO KAI NING","LIU WENXI","LU JINYU","LU YANRAN","MA JINXIN","NEO TSIAO EN SOPHIE AKHTAR","PHUA JIA QING ZACHARY ASHLEY","RHYS WEI PATTON","SHARVIN KARTHIK","TAN YONG ZHI","VIHAAN VIGNESWARAN","WONG WEI JUN ZACHARY","ZHANG XINYU","ZHANG YAXUAN","ZONG QICHEN DYLAN"]
+
+# extra 2
+
+ans=input("What do you call a bear withour a ear?")
+hidden = "b"
+HIDDEN = "B"
+if ans == hidden or ans == HIDDEN:
+    print("Correct")
+else:
+    print("Wrong")
+
